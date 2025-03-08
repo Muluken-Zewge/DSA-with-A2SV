f3e60b9edd6cd86c2dc5ff63f875d89c5f8ffa99
Muluken Zewge <mulukenzewge@gmail.com>
2025-03-08 10:29:34 +0300

    Created 'Third Maximum Number' by A2SV Hub

---
+++ 08-Mar-2025/Third Maximum Number 297822.py
@@ -0,0 +1,11 @@
+# Problem: Third Maximum Number - https://leetcode.com/problems/third-maximum-number/description/
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        unique_nums = list(set(nums)) # to remove duplicates
+        unique_nums.sort()
+
+        if len(unique_nums) >= 3:
+            return unique_nums[-3]
+        else:
+            return unique_nums[-1]
