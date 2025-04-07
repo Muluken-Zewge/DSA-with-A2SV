b54c1d384efe1dfbfdeafbc093531303fe741a6c
Muluken Zewge <mulukenzewge@gmail.com>
2025-04-07 16:16:21 +0300

    Created 'Kefa and Company' by A2SV Hub

---
+++ 07-Apr-2025/Kefa and Company 327886.py
@@ -0,0 +1,26 @@
+# Problem: Kefa and Company - https://codeforces.com/problemset/problem/580/B
+
+def main():
+    n, d = map(int, input().split())
+    friends = []
+    for _ in range(n):
+        m, s = map(int, input().split())
+        friends.append([m,s])
+
+    friends.sort()
+    current_friend_factor = 0
+    max_friend_factor = 0
+    left = 0 
+
+    for right in range(len(friends)):
+        current_friend_factor += friends[right][1]
+
+        while friends[right][0] - friends[left][0] > d:
+            current_friend_factor -= friends[left][1]
+            left += 1
+
+        max_friend_factor = max(max_friend_factor, current_friend_factor)
+
+    print(max_friend_factor)
+
+main()
