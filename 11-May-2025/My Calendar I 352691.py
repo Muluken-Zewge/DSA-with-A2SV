df33f1e05cf93f79e5a4fe8e54093b06a6dffb55
Muluken Zewge <mulukenzewge@gmail.com>
2025-05-12 16:12:46 +0300

    Update My Calendar I 352691.py
    added binary search version

--- 11-May-2025/My Calendar I 352691.py
+++ 11-May-2025/My Calendar I 352691.py
@@ -13,3 +13,29 @@ class MyCalendar:
         self.bookings.append((start, end))
         
         return True
+
+#binary search version
+class MyCalendar:
+
+    def __init__(self):
+        self.bookings = []
+
+    def book(self, start: int, end: int) -> bool:
+        idx = bisect_right(self.bookings, (start,end))
+
+        #check overlap with the previous interval
+        if idx > 0:
+            prev_start, prev_end = self.bookings[idx-1]
+            if start < prev_end:
+                return False
+
+        #check overlap with the next interval
+        #if idx == len(self.bookings),it will be added at the end
+        if idx < len(self.bookings):
+            next_start, next_end = self.bookings[idx]
+            if end > next_start:
+                return False
+        
+        self.bookings.insert(idx, (start,end))
+
+        return True
