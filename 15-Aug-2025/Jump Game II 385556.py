bdfb3a0131c96e979b64cef457656cd26cd604ea
Muluken Zewge <mulukenzewge@gmail.com>
2025-08-15 21:21:36 +0300

    Created 'Jump Game II' by A2SV Hub

---
+++ 15-Aug-2025/Jump Game II 385556.py
@@ -0,0 +1,14 @@
+# Problem: Jump Game II - https://leetcode.com/problems/jump-game-ii/description/
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [float(inf)] * n
+        dp[0] = 0
+        for i in range(1, n):
+            for j in range(i):
+                if nums[j] >= (i - j):
+                    dp[i] = min(dp[i], dp[j] + 1)
+
+        return dp[n-1]
+
