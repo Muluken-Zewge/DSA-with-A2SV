2d7494abf8db38579def3515c8ce5bad5d11cbb2
Muluken Zewge <mulukenzewge@gmail.com>
2025-08-28 21:28:05 +0300

    Created 'Restore IP Addresses' by A2SV Hub

---
+++ 28-Aug-2025/Restore IP Addresses 389632.py
@@ -0,0 +1,27 @@
+# Problem: Restore IP Addresses - https://leetcode.com/problems/restore-ip-addresses/
+
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        res = []
+        def backtrack(start, path):
+            # If we have 4 parts and used all digits -> valid IP
+            if len(path) == 4:
+                if start == len(s):
+                    res.append(".".join(path))
+                return
+
+            # Try segments of length 1 to 3
+            for length in range(1, 4):
+                if start + length > len(s):
+                    break
+                segment = s[start:start+length]
+
+                # Skip invalid segments
+                if (segment.startswith("0") and len(segment) > 1) or int(segment) > 255:
+                    continue
+
+                backtrack(start + length, path + [segment])
+
+        backtrack(0, [])
+        return res
+
