944697100fabdd457f78cd644e94583dc1e5c077
Muluken Zewge <mulukenzewge@gmail.com>
2025-10-31 16:10:12 +0300

    Created 'Integer to Roman' by A2SV Hub

---
+++ 31-Oct-2025/Integer to Roman 406670.py
@@ -0,0 +1,23 @@
+# Problem: Integer to Roman - https://leetcode.com/problems/integer-to-roman/description/
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        # greedy mapping method
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        ]
+        syms = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        ]
+        res = ""
+        
+        for i in range(len(val)):
+            count = num // val[i]
+            res += syms[i] * count
+            num %= val[i]
+        
+        return res
